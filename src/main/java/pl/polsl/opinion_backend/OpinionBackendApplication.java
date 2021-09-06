f8bfde6bf472a9b45d4840ccf5e28ce92c089024
Patryk Osysek <patryk.osysek@itsilesia.com>
2021-09-06 14:27:45 +0200

    Initial commit

---
+++ src/main/java/pl/polsl/opinion_backend/OpinionBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.opinion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpinionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/polsl/opinion_backend/OpinionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.opinion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
